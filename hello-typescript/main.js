58ee1daf33e14665bc4b4163ed95bd69722610d3
Wesley Nunes de Lima <nunes.wesley@hotmail.com>
2017-10-26 19:01:10 -0200

    All files

---
+++ hello-typescript/main.js
@@ -0,0 +1,22 @@
+var minhaVAr = 'minha variavel';
+function minhaFuncao(x, y) {
+    return x + y;
+}
+//ES 6 ou ES 2015
+var num = 2;
+var pi = 3.14;
+var numeros = [1, 2, 3];
+numeros.map(function (valor) {
+    return valor * 2;
+});
+numeros.map(function (valor) { return valor * 2; }); //ES2015
+var Matematica = /** @class */ (function () {
+    function Matematica() {
+    }
+    Matematica.prototype.soma = function (x, y) {
+        return x + y;
+    };
+    return Matematica;
+}());
+var n1 = 'teste';
+n1 = 4;
---
+++ primeiro-projeto/src/app/meu-primeiro/meu-primeiro.component.ts
@@ -0,0 +1,12 @@
+import {Component} from '@angular/core';
+
+@Component({
+    selector: 'meu-primeiro-component',
+    template: `
+        <p>Meu primeiro componente com angular2</p>
+    `
+})
+
+export class MeuPrimeiroComponent{
+
+}
