d1392426ab6a632e90450ec81873261b86bd7626
Wesley Nunes <nunes.wesley@hotmail.com>
2017-10-31 00:48:21 -0200

    Aula8

--- primeiro-projeto/src/app/cursos/cursos.component.ts
+++ primeiro-projeto/src/app/cursos/cursos.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+import { CursosService } from './cursos.service';
+
 @Component({
   selector: 'app-cursos',
   templateUrl: './cursos.component.html',
@@ -7,7 +9,21 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CursosComponent implements OnInit {
 
-  constructor() { }
+  nomePortal: string;
+
+  cursos: string [];
+
+  constructor(private cursosService: CursosService) { 
+    this.nomePortal = 'http://loiane.training';
+
+    /*for(let i=0; i<this.cursos.length;i++ ){
+      let curso = this.cursos[i];
+    }*/
+
+    this.cursos = this.cursosService.getCursos(); 
+   }
+
+
 
   ngOnInit() {
   }
--- primeiro-projeto/src/app/cursos/cursos.module.ts
+++ primeiro-projeto/src/app/cursos/cursos.module.ts
@@ -1,3 +1,4 @@
+import { CursosService } from './cursos.service';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CursosComponent } from './cursos.component';
@@ -13,6 +14,9 @@ import { CursoDetalheComponent } from './curso-detalhe/curso-detalhe.component';
   ],
   exports: [
     CursosComponent
+  ],
+  providers:[
+    CursosService
   ]
 })
 export class CursosModule { }
