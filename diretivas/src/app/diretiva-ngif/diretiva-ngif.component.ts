6dc7532c6642c4b4f8a7f40dbd916a51e308fd43
Wesley Nunes <nunes.wesley@hotmail.com>
2017-11-10 23:59:19 -0200

    Aula25 - diretivas ngIf

--- diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
+++ diretivas/src/app/diretiva-ngif/diretiva-ngif.component.ts
@@ -7,9 +7,16 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DiretivaNgifComponent implements OnInit {
 
+  cursos : string[] = ["Angular2"];
+
+  mostrarCursos : boolean = false;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  onMostrarCursos(){
+    this.mostrarCursos = !this.mostrarCursos;
+  }
 }
