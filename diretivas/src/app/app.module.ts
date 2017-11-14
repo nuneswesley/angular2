525dc8ec382c53f1713ffb93bc2fbfd53175ece1
Wesley Nunes <nunes.wesley@hotmail.com>
2017-11-13 23:12:40 -0200

    Aula26 - ngSwitch, ngSwitchCase and ngSwitchDefault

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -1,17 +1,20 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { TesteComponent } from './teste/teste.component';
+import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
-    TesteComponent
+    TesteComponent,
+    DiretivaNgswitchComponent
   ],
   imports: [
+    AlertModule.forRoot(),
     BrowserModule
   ],
   providers: [],
