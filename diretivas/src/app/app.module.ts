edc1035525a8cc4874a6d644e1893f5d6abc96c1
Wesley Nunes <nunes.wesley@hotmail.com>
2017-11-14 09:54:31 -0200

    Aula27 - *ngFor

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -4,12 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { TesteComponent } from './teste/teste.component';
+import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
-    TesteComponent
+    TesteComponent,
+    DiretivaNgforComponent
   ],
   imports: [
     BrowserModule
