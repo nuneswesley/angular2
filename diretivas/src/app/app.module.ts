4415941a9a89de22e9cb4c60e7fb11e89b0548ba
Wesley Nunes <nunes.wesley@hotmail.com>
2017-12-05 15:16:26 -0200

    Aula30 - ngStyle

--- diretivas/src/app/app.module.ts
+++ diretivas/src/app/app.module.ts
@@ -2,30 +2,30 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { AlertModule } from 'ngx-bootstrap';
 import { AppComponent } from './app.component';
+import { FormsModule } from '@angular/forms';
+
 import { DiretivaNgifComponent } from './diretiva-ngif/diretiva-ngif.component';
 import { TesteComponent } from './teste/teste.component';
-<<<<<<< HEAD
 import { DiretivaNgforComponent } from './diretiva-ngfor/diretiva-ngfor.component';
-=======
 import { DiretivaNgswitchComponent } from './diretiva-ngswitch/diretiva-ngswitch.component';
 import { DiretivaNgclassComponent } from './diretiva-ngclass/diretiva-ngclass.component';
->>>>>>> efc77bdd1285910a572d1bccf80ea8b55253089b
+import { DiretivaNgstyleComponent } from './diretiva-ngstyle/diretiva-ngstyle.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
     DiretivaNgifComponent,
     TesteComponent,
-<<<<<<< HEAD
-    DiretivaNgforComponent
-=======
+    DiretivaNgforComponent,
     DiretivaNgswitchComponent,
-    DiretivaNgclassComponent
->>>>>>> efc77bdd1285910a572d1bccf80ea8b55253089b
+    DiretivaNgclassComponent,
+    DiretivaNgstyleComponent
   ],
   imports: [
     AlertModule.forRoot(),
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
