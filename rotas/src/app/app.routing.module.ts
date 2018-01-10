1bff1027c4efb0906c2c89b60741b4bb746a1bb8
Wesley Nunes <nunes.wesley@hotmail.com>
2018-01-10 00:09:50 -0200

    Aula59 - Criando um módulo de rotas de funcionalidade

--- rotas/src/app/app.routing.module.ts
+++ rotas/src/app/app.routing.module.ts
@@ -3,17 +3,17 @@ import { ModuleWithProviders } from '@angular/core'
 import { Routes,RouterModule } from '@angular/router'
 
 import { HomeComponent } from './home/home.component';
-import { CursosComponent } from './cursos/cursos.component';
+//import { CursosComponent } from './cursos/cursos.component';
 import { LoginComponent } from './login/login.component';
-import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
-import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
+//import { CursoDetalheComponent } from './cursos/curso-detalhe/curso-detalhe.component';
+//import { CursoNaoEncontradoComponent } from './cursos/curso-nao-encontrado/curso-nao-encontrado.component';
 
 
 const appRoutes : Routes =[
-    { path: 'cursos', component: CursosComponent },
-    { path: 'curso/:id', component: CursoDetalheComponent },
+    //{ path: 'cursos', component: CursosComponent },
+    //{ path: 'curso/:id', component: CursoDetalheComponent },
     { path: 'login', component: LoginComponent },
-    { path: 'cursoNaoEncontrado', component: CursoNaoEncontradoComponent },
+    //{ path: 'cursoNaoEncontrado', component: CursoNaoEncontradoComponent },
     { path: '', component: HomeComponent }
 ] 
 
